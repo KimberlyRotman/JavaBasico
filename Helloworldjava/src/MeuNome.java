d9a6b9e4223e69fc89e09cefb01cfbf0fd7ecf85
KimberlyRotman <kimberlyrotmanms@gmail.com>
2024-05-09 16:04:04 -0300

    adição do arquivo MeuNome

---
+++ Helloworldjava/src/MeuNome.java
@@ -0,0 +1,18 @@
+public class MeuNome {
+
+    public static void main(String[] args) {
+
+        String primeiroNome = "Kimberly";
+        String segundoNome = "Rotman";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+
+        return "Meu nome é: " + primeiroNome.concat(" ").concat(segundoNome);
+
+    }
+
+}
